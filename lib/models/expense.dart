ae0090e656161e8bae482b99d484b01c88f7dfaa
karim <karim.tarek96@yahoo.com>
2024-04-22 17:40:01 +0200

    added new_expense.dart file

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,12 +1,23 @@
 // Expense Data Structure for the app
 
 import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
+
+final formatter = DateFormat.yMd();
 
 // random expense id
 const uuid = Uuid();
 
 // expense category enum
 enum Category { food, travel, leisure, work }
+
+const Map<Category, IconData> categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+};
 
 class Expense {
   // Class Constructor
@@ -24,4 +35,8 @@ class Expense {
   final String title;
   final double amount;
   final Category category;
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
 }
